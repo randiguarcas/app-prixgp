1f9d2e14cc96f25c0ef487f9de1d95e8d993b748
Randi Guarcas <rguarcas18@gmail.com>
2023-01-26 00:51:00 -0600

    Added Facebook login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-// import auth from "@react-native-firebase/auth";
-// import {
-//   GoogleSignin,
-//   GoogleSigninButton,
-// } from "@react-native-google-signin/google-signin";
-
+import auth from "@react-native-firebase/auth";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import {
+  LoginManager,
+  AccessToken,
+  LoginButton,
+} from "react-native-fbsdk-next";
 import {
   Center,
   Box,
@@ -27,28 +32,56 @@ type LoginStackNavigationProp = NativeStackNavigationProp<
   "signup"
 >;
 
-// GoogleSignin.configure({
-//   webClientId:
-//     "27550352723-99o15re9mgko4mt8i4j8eq7094hd6901.apps.googleusercontent.com",
-//   scopes: ["https://www.googleapis.com/auth/drive.readonly"],
-// });
-
-
 export default function LoginScreen() {
-  console.log()
+  GoogleSignin.configure({
+    webClientId:
+      "27550352723-99o15re9mgko4mt8i4j8eq7094hd6901.apps.googleusercontent.com",
+  });
+
   // const navigation = useNavigation<LoginStackNavigationProp>();
 
-  // const handleGoogleAuth = async () => {
-  //   try {
-  //     await GoogleSignin.hasPlayServices();
-  //     const { idToken } = await GoogleSignin.signIn();
-  //     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  const handleGoogleAuth = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-  //     return auth().signInWithCredential(googleCredential);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+      return auth().signInWithCredential(googleCredential);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onFacebookButtonPress = async () => {
+    try {
+      // Attempt login with permissions
+      const result = await LoginManager.logInWithPermissions([
+        "public_profile",
+        "email",
+      ]);
+
+      if (result.isCancelled) {
+        throw "User cancelled the login process";
+      }
+
+      // Once signed in, get the users AccesToken
+      const data = await AccessToken.getCurrentAccessToken();
+
+      if (!data) {
+        throw "Something went wrong obtaining access token";
+      }
+
+      // Create a Firebase credential with the AccessToken
+      const facebookCredential = auth.FacebookAuthProvider.credential(
+        data.accessToken
+      );
+
+      // Sign-in the user with the credential
+      return auth().signInWithCredential(facebookCredential);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <Center w="100%">
@@ -95,14 +128,32 @@ export default function LoginScreen() {
               Recuperar contraseña
             </Link>
           </FormControl>
-          <Button mt="2" colorScheme="indigo">
-            Ingresar
+          <Button mt="2" colorScheme="lightBlue">
+            Iniciar sesión
           </Button>
-          {/* <GoogleSigninButton
-            style={{ width: 192, height: 48 }}
+          <GoogleSigninButton
+            style={{ width: 338, height: 60, marginLeft: -5 }}
             size={GoogleSigninButton.Size.Wide}
             color={GoogleSigninButton.Color.Dark}
-          /> */}
+            onPress={handleGoogleAuth}
+          />
+          <LoginButton
+            style={{ width: 333, height: 50 }}
+
+            onLoginFinished={(error, result) => {
+              if (error) {
+                console.log("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                console.log("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then((data) => {
+                  console.log(data.accessToken.toString());
+                });
+              }
+            }}
+            onLogoutFinished={() => console.log("logout.")}
+          />
+          {/* <Button onPress={onFacebookButtonPress}>Acceder con Facebook</Button> */}
           <HStack mt="6" justifyContent="center">
             <Link
               _text={{
